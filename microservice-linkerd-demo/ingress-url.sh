1e76ac98b92f6f272917ac35e3d34b087a00bc9e
Eberhard Wolff <eberhard.wolff@gmail.com>
2020-08-28 19:37:02 +0200

    Add traefik namespace to shell skript

--- microservice-linkerd-demo/ingress-url.sh
+++ microservice-linkerd-demo/ingress-url.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-IP=$(kubectl get service traefik -o jsonpath='{.status.loadBalancer.ingress[0].hostname}') 
-PORT=$(kubectl get service traefik -o jsonpath='{.spec.ports[?(@.name=="web")].port}')
+IP=$(kubectl get service traefik -n traefik -o jsonpath='{.status.loadBalancer.ingress[0].hostname}') 
+PORT=$(kubectl get service traefik -n traefik -o jsonpath='{.spec.ports[?(@.name=="web")].port}')
 echo http://$IP:$PORT/
